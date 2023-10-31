3be119fde76cf3ea42d1436f9f442d91e2cd0292
Tasneem <tasneemgazal5@gmail.com>
2023-10-31 21:15:05 +0200

    create Onion.jsx file

---
+++ src/components/onion/Onion.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState ,useEffect } from 'react'
+import '../dishes.css'
+export default function Onion() {
+    let[onion,setOnion]=useState([]);
+    const getOnion = async()=>{
+        let response = await fetch("https://forkify-api.herokuapp.com/api/search?q=onion");
+        let data =await response.json();
+        setOnion(data.recipes);
+        console.log(data.recipes)
+    }
+    useEffect( ()=>{
+        getOnion();
+    },[]);
+      return (
+    <div className="container">
+    <div className='row'>
+        {onion.map((dish)=>{
+
+            return <div className='dish col-md-4 my-5'>
+                <div className='dish '>
+                <h2 className='py-2'>{dish.title}</h2>
+                <img src={dish.image_url} className='w-100' />
+                </div>
+            </div>
+            
+        })}
+
+    </div>
+    </div>
+  )
+}
