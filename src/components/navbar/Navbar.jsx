5c88f75a91b74008be76451b4faad1d38d7b3dc0
Tasneem <tasneemgazal5@gmail.com>
2023-10-31 21:13:48 +0200

    add padding to navbar & update Restueant link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,14 +5,14 @@ import Navbar from 'react-bootstrap/Navbar';
 import {Link} from 'react-router-dom'
 function customNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary py-4">
       <Container>
         <Navbar.Brand href="#home">API Task</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Link  className='me-2' to='/'>Home</Link>
-            <Link  className='me-2' to='/Resturant'>Resturant</Link>
+            <Link  className='me-2' to='/resturant'>Resturant</Link>
             <Link className='me-2' to='/products'>Products</Link>
           </Nav>
         </Navbar.Collapse>
