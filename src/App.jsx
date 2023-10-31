224f3303c6821e1ee5037fb002113145b87b5731
Tasneem <tasneemgazal5@gmail.com>
2023-10-31 21:11:31 +0200

    import Resturant.jsx file in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar.jsx'
 import Products from './components/products/Products.jsx'
+import Resturant from './components/resturant/Resturant.jsx'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 export default function App() {
   return (
+    <>
     <BrowserRouter>
     <Navbar/>
     <Routes>
     <Route path='/products' element={<Products/>}/>
+    <Route path='/resturant' element={<Resturant/>}/>
     </Routes>
    
     </BrowserRouter>
+    </>
   )
 }
 
