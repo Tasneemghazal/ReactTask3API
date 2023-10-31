2721e66cddd3a16f0103c951b0c507e097000570
Tasneem <tasneemgazal5@gmail.com>
2023-10-31 18:12:58 +0200

    fetching data from api link in products function

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
-
+import { useState ,useEffect } from 'react'
 export default function Products() {
-  return (
-    <div>Products</div>
+    let[products,setProducts]=useState([]);
+    const getProducts = async()=>{
+        let response = await fetch("https://fakestoreapi.com/products");
+        let data =await response.json();
+        setProducts(data);
+    }
+    useEffect( ()=>{
+        getProducts();
+    },[]);
+      return (
+    <div className="container">
+    <div className='row'>
+        {products.map((product)=>{
+
+            return <div className='col-md-4 '>
+                <div className='pro'>
+                <h2>{product.title}</h2>
+                <img src={product.image} className='w-50'/>
+                </div>
+            </div>
+            
+        })}
+
+    </div>
+    </div>
   )
 }
