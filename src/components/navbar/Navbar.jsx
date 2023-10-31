b566548c0c53c266d413c762fd56a98868c6dec3
Tasneem <tasneemgazal5@gmail.com>
2023-10-31 18:13:45 +0200

    use navbar comp. from react bootstrap

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-export default function Navbar() {
+import {Link} from 'react-router-dom'
+function customNavbar() {
   return (
-    <div>Navbar</div>
-  )
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">API Task</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Link  className='me-2' to='/'>Home</Link>
+            <Link  className='me-2' to='/Resturant'>Resturant</Link>
+            <Link className='me-2' to='/products'>Products</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default customNavbar;
