426f82e42dcb8877b2f5276b8d1042b080833abb
Tasneem <tasneemgazal5@gmail.com>
2023-11-01 16:44:17 +0200

    add bootstrap classes

--- src/components/resturant/Resturant.jsx
+++ src/components/resturant/Resturant.jsx
@@ -5,16 +5,16 @@ import Onion from '../onion/Onion.jsx'
 export default function Resturant() {
   return (
     <>
-    <div className='my-5 text-center '>
+    <div className='my-5 text-center py-5'>
         <h1>Pizza</h1>
         <Pizza/>
     </div>
    
-    <div className='my-5 text-center bg-body-tertiary'>
+    <div className='my-5 text-center bg-body-tertiary py-5'>
         <h1>Salad</h1>
         <Salad/>
     </div>
-    <div className='my-5 text-center'>
+    <div className='my-5 text-center py-5'>
         <h1>Onion</h1>
         <Onion/>
     </div>
